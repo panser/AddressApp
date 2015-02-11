acfa2f91e3f397f3aad1bc60d1ff95b17c0834ea
Sergey Panov <gostroy@gmail.com>
2015-02-11 13:10:46 +0200

    add icon to project ( http://code.makery.ch/library/javafx-8-tutorial/ru/part4/ )

--- src/ua/org/gostroy/adress/MainApp.java
+++ src/ua/org/gostroy/adress/MainApp.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -48,6 +49,10 @@ public class MainApp extends Application {
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("AddressApp");
+
+		// Set the application icon.
+		this.primaryStage.getIcons().add(
+				new Image("file:resources/images/address_book_32.png"));
 
 		initRootLayout();
 
@@ -133,6 +138,9 @@ public class MainApp extends Application {
 			dialogStage.initOwner(primaryStage);
 			Scene scene = new Scene(page);
 			dialogStage.setScene(scene);
+
+			dialogStage.getIcons().add(
+					new Image("file:resources/images/address_book_32.png"));
 
 			// Set the person into the controller.
 			PersonEditDialogController controller = loader.getController();
