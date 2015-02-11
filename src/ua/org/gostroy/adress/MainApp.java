a9dd5af48e498f47ba70d5ee4be84d34b4867fc4
Sergey Panov <gostroy@gmail.com>
2015-02-11 08:59:17 +0200

    add action on New... and Edit... button (sample from http://code.makery.ch/library/javafx-8-tutorial/ru/part3/ )

--- src/ua/org/gostroy/adress/MainApp.java
+++ src/ua/org/gostroy/adress/MainApp.java
@@ -3,16 +3,46 @@ package ua.org.gostroy.adress;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import ua.org.gostroy.adress.model.Person;
+import ua.org.gostroy.adress.view.PersonEditDialogController;
+import ua.org.gostroy.adress.view.PersonOverviewController;
 
 public class MainApp extends Application {
 
 	private Stage primaryStage;
 	private BorderPane rootLayout;
+	private ObservableList<Person> personData = FXCollections
+			.observableArrayList();
+
+	public MainApp() {
+		// Add some sample data
+		personData.add(new Person("Hans", "Muster"));
+		personData.add(new Person("Ruth", "Mueller"));
+		personData.add(new Person("Heinz", "Kurz"));
+		personData.add(new Person("Cornelia", "Meier"));
+		personData.add(new Person("Werner", "Meyer"));
+		personData.add(new Person("Lydia", "Kunz"));
+		personData.add(new Person("Anna", "Best"));
+		personData.add(new Person("Stefan", "Meier"));
+		personData.add(new Person("Martin", "Mueller"));
+	}
+
+	/**
+	 * Returns the data as an observable list of Persons.
+	 * 
+	 * @return
+	 */
+	public ObservableList<Person> getPersonData() {
+		return personData;
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -57,6 +87,10 @@ public class MainApp extends Application {
 
 			// Set person overview into the center of root layout.
 			rootLayout.setCenter(personOverview);
+
+			// Give the controller access to the main app.
+			PersonOverviewController controller = loader.getController();
+			controller.setMainApp(this);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -74,4 +108,45 @@ public class MainApp extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	/**
+	 * Opens a dialog to edit details for the specified person. If the user
+	 * clicks OK, the changes are saved into the provided person object and true
+	 * is returned.
+	 * 
+	 * @param person
+	 *            the person object to be edited
+	 * @return true if the user clicked OK, false otherwise.
+	 */
+	public boolean showPersonEditDialog(Person person) {
+		try {
+			// Load the fxml file and create a new stage for the popup dialog.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class
+					.getResource("view/PersonEditDialog.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+
+			// Create the dialog Stage.
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Edit Person");
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.initOwner(primaryStage);
+			Scene scene = new Scene(page);
+			dialogStage.setScene(scene);
+
+			// Set the person into the controller.
+			PersonEditDialogController controller = loader.getController();
+			controller.setDialogStage(dialogStage);
+			controller.setPerson(person);
+
+			// Show the dialog and wait until the user closes it
+			dialogStage.showAndWait();
+
+			return controller.isOkClicked();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
 }
