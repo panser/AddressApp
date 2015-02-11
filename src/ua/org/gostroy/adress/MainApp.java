41ac37848038e4b0e719927415c55bd2b337dafe
Sergey Panov <gostroy@gmail.com>
2015-02-11 08:59:17 +0200

    make sample from http://code.makery.ch/library/javafx-8-tutorial/ru/part2/

--- src/ua/org/gostroy/adress/MainApp.java
+++ src/ua/org/gostroy/adress/MainApp.java
@@ -3,16 +3,44 @@ package ua.org.gostroy.adress;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import ua.org.gostroy.adress.model.Person;
+import ua.org.gostroy.adress.view.PersonOverviewController;
 
 public class MainApp extends Application {
 
 	private Stage primaryStage;
 	private BorderPane rootLayout;
+	private ObservableList<Person> personData = FXCollections
+			.observableArrayList();
+
+	public MainApp() {
+		// Add some sample data
+		personData.add(new Person("Hans", "Muster"));
+		personData.add(new Person("Ruth", "Mueller"));
+		personData.add(new Person("Heinz", "Kurz"));
+		personData.add(new Person("Cornelia", "Meier"));
+		personData.add(new Person("Werner", "Meyer"));
+		personData.add(new Person("Lydia", "Kunz"));
+		personData.add(new Person("Anna", "Best"));
+		personData.add(new Person("Stefan", "Meier"));
+		personData.add(new Person("Martin", "Mueller"));
+	}
+
+	/**
+	 * Returns the data as an observable list of Persons.
+	 * 
+	 * @return
+	 */
+	public ObservableList<Person> getPersonData() {
+		return personData;
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -57,6 +85,10 @@ public class MainApp extends Application {
 
 			// Set person overview into the center of root layout.
 			rootLayout.setCenter(personOverview);
+
+			// Give the controller access to the main app.
+			PersonOverviewController controller = loader.getController();
+			controller.setMainApp(this);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
